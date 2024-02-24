d6c9a5d662a31683a4a66b7f0e4132a01db12c52
vineetk13 <vineetkme.99@gmail.com>
2024-02-24 15:06:15 +0530

    fix: authed user login redirect

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("", include("django.contrib.auth.urls")),
-    path("login/", auth_views.LoginView.as_view(redirect_authenticated_user=True), name="login")
+    path("login/", auth_views.LoginView.as_view(redirect_authenticated_user=True), name="login"),
+    path("", include("django.contrib.auth.urls"))
 ]
 
 # "django.contrib.auth.urls" gives access to all of the below:s
